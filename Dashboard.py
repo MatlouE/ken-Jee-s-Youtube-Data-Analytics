4b52c83c28e68bf6d94b064cbc83badcd2879f05
MatlouE <149234501+MatlouE@users.noreply.github.com>
2026-01-05 04:04:25 +0000

    modified:   Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -31,20 +31,20 @@ def load_data():
                       'Subscribers_lost','Subscribers_gained','RPM(USD)','CPM(USD)','Average_%_viewed','Average_view_duration',
                       'Views','Watch_time (hours)','Subscribers','Your_estimated_revenue(USD)','Impressions','Impressions_ctr(%)']
     
-    df_agg['Video_publish_time'] = pd.to_datetime(df_agg['Video_publish_time'])#converting to datetime
+    df_agg['Video_publish_time'] = pd.to_datetime(df_agg['Video_publish_time'], format='mixed')#converting to datetime
     
     df_agg['Average_view_duration'] = df_agg['Average_view_duration'].apply(lambda x: datetime.strptime(x, '%H:%M:%S'))
     df_agg['Avg_duration_sec'] = df_agg['Average_view_duration'].apply(lambda x: x.second + x.minute*60 + x.hour*3600)
     
-    df_agg['Engagement_ratio'] = (df_agg['Comments_added'] + df_agg['Shares'] + df_agg['Dislike'] + df_agg['Likes']) /df_agg.Views # Engagement out of total views
-    df_agg['Views/sub_gained'] = df_agg['Views'] / df_agg['Subscribers gained'] #views vs subs gained ratio
+    df_agg['Engagement_ratio'] = (df_agg['Comments_added'] + df_agg['Shares'] + df_agg['Dislikes'] + df_agg['Likes']) /df_agg.Views # Engagement out of total views
+    df_agg['Views/sub_gained'] = df_agg['Views'] / df_agg['Subscribers_gained'] #views vs subs gained ratio
     df_agg.sort_values('Video_publish_time', ascending= False, inplace=True)
     
     #other datasets
     df_agg_sub = pd.read_csv("Aggregated_Metrics_By_Country_And_Subscriber_Status.csv")
     df_comments = pd.read_csv("All_Comments_Final.csv")
     df_time = pd.read_csv("Video_Performance_Over_Time.csv")
-    df_time['Date'] = pd.to_datetime(df_time['Date'])
+    df_time['Date'] = pd.to_datetime(df_time['Date'], format='mixed')
     
     return df_agg, df_agg_sub, df_comments, df_time
 #basically returning our feature engineered dataframes
@@ -54,28 +54,38 @@ df_agg, df_agg_sub, df_comments, df_time = load_data()
 #additional data engineering for aggregatted data
 df_agg_diff = df_agg.copy()
 metric_date_12mo = df_agg_diff['Video_publish_time'].max() - pd.DateOffset(months=12)
-median_agg = df_agg_diff[df_agg_diff['Video_publish_time'] >= metric_date_12mo].median()
-    
+
+# Filter rows
+filtered_df = df_agg_diff[df_agg_diff['Video_publish_time'] >= metric_date_12mo]
+
+# Select only numeric columns
+numeric_cols = filtered_df.select_dtypes(include=[np.number])
+
+# Compute median across rows (axis=0 → column-wise median)
+median_agg = np.median(numeric_cols, axis=0)
 #create differences from the median for values
 #just numeric columns
-numeric_cols = np.array((df_agg_diff.dtypes == 'float64') | df_agg_diff.dtypes == 'int64')#an array of the numeric columns
-df_agg_diff.iloc[:, numeric_cols] = (df_agg_diff.iloc[:, numeric_cols] - median_agg).div(median_agg)
-
 #merge daily data with publish data to get delta
 #merging df_time dataset with df_agg but just the two related columns as foreign keys
-df_time_diff  = pd.merge(df_time, df_agg.loc[:, ['Video', 'Video_publish_time']], left_on ='External_ID', right_on = 'Video')
-df_time_diff['days_pubished'] = (df_time_diff['Date'] - df_time_diff['Video_publish_time']).dt.days
+df_time_diff = pd.merge(df_time, df_agg,left_on = 'External Video ID', right_on='Video', how='left')
+df_time_diff['days_published'] = (df_time_diff['Date'] - df_time_diff['Video_publish_time']).dt.days
 
 #get last 12months of data rather than all data
-date_12mo = df_agg['Video publish time'].max() - pd.DateOffset(months=12)
-df_time_diff_yr =  df_time_diff[df_time_diff['Video publish time'] >= date_12mo]
+date_12mo = df_agg['Video_publish_time'].max() - pd.DateOffset(months=12)
+df_time_diff_yr =  df_time_diff[df_time_diff['Video_publish_time'] >= date_12mo]
                                  
 #get daily view data first 30 median & percentiles
-views_days = pd.pivot(df_time_diff_yr, index='days_published', values = 'Views', aggfunc = [np.mean, np.median, lambda x: np.percentile(x, 80), lambda x: np.percentile(x, 20)]).reset_index()
+views_days = pd.pivot_table(
+    df_time_diff_yr,
+    index='days_published',
+    values='Views_x',
+    aggfunc=[np.mean, np.median, lambda x: np.percentile(x, 80), lambda x: np.percentile(x, 20)]
+).reset_index()
+
 views_days.columns = ['days_published','mean_views','median_views','80pct_views','20pct_views']
-views_days = views_days[views_days['days_published'].between(0,30)]
+views_days = views_days[views_days['days_published'].between(0, 30)]
 views_cumulative = views_days.loc[:, ['days_published', 'median_views','80pct_views', '20pct_views']]
-views_cumulative.loc[:, ['median_views', '80pct_views', '20pct_views']] = views_cumulative.loc[:, ['median_values', '80pct_views', '20pct_views']].cumsum()
+views_cumulative.loc[:, ['median_views', '80pct_views', '20pct_views']] = views_cumulative.loc[:, ['median_views', '80pct_views', '20pct_views']].cumsum()
 
 
 #Building Our Streamlit app
@@ -84,14 +94,31 @@ views_cumulative.loc[:, ['median_views', '80pct_views', '20pct_views']] = views_
 add_sidebar = st.sidebar.selectbox('Aggregate or Individual Video', ('Aggregate Metrics', 'Individual Video Analysis'))
 
 #show individual Metrics
-if add_sidebar == 'Aggregatee Metrics':
+if add_sidebar == 'Aggregate Metrics':
     st.write("Ken Lee YouTube Aggregated Data")
     
-    df_agg_metrics = df_agg[['Video publish time','Views','Likes','Subscribers','Shares','Comments added','RPM(USD)','Average % viewed',
-                             'Avg_duration_sec', 'Engagement_ratio','Views / sub gained']]
+    ##Getting metrics to show on our Dashboard
+    df_agg_metrics = df_agg[['Video_publish_time','Views','Likes','Subscribers','Shares','Comments_added','RPM(USD)','Average_%_viewed',
+                             'Avg_duration_sec', 'Engagement_ratio','Views/sub_gained']]
+    
+    #calculate medians for last 6 and 12 months
     metric_date_6mo = df_agg_metrics['Video_publish_time'].max() - pd.DateOffset(months=6)
     metric_date_12mo = df_agg_metrics['Video_publish_time'].max() - pd.DateOffset(months=12)
     metric_medians6mo = df_agg_metrics[df_agg_metrics['Video_publish_time'] >= metric_date_6mo].median()
     metric_medians12mo = df_agg_metrics[df_agg_metrics['Video_publish_time'] >= metric_date_12mo].median()
     
-    
+    col1, col2, col3, col4, col5 = st.columns(5)
+    columns = [col1, col2, col3, col4, col5]
+
+    count = 0
+    for i in metric_medians6mo.index:
+        with columns[count]:
+            delta = (metric_medians6mo[i] - metric_medians12mo[i])/metric_medians12mo[i] #delta - the difference between two values in
+            # this case delta is the percentage change between the 6month median and 12 month median
+            st.metric(label=i, value= f"{metric_medians6mo[i]:.2f}", delta= f"{delta:.2%}")
+            count += 1
+            if count >= 5:
+                count = 0
+
+if add_sidebar == 'Individual Video Analysis':
+    st.write("Ken Lee YouTube Aggregated Data")
